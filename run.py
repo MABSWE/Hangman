dd2b11446300812d40e74fb2b8b271a5e3f012ad
MABSWE <>
2024-01-31 10:01:42 +0000

    Add random words

--- run.py
+++ run.py
@@ -3,3 +3,5 @@ import random
 print("Welcome to Hangman!")
 print("--------------------")
 
+# Random words
+wordPulp = ["monkey", "apple", "computer", "tank", "helicopter", "carrot", "sunshine", "elevator"]
