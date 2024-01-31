784cd171e9a6e607a07841f824a1fdeed2e4aa63
MABSWE <>
2024-01-31 13:51:47 +0000

    Fix additional blank spaces

--- run.py
+++ run.py
@@ -26,79 +26,84 @@ for x in randomWord:
             print("     |")
             print("     |")
             print("   -----")
-    elif wrong == 1:
-        print("\n----")
-        print("  |  |")
-        print("     |")
-        print("     |")
-        print("     |")
-        print("     |")
-        print("   -----")
-    elif wrong == 2:
-        print("\n----")
-        print("  |  |")
-        print("  O  |")
-        print("     |")
-        print("     |")
-        print("     |")
-        print("   -----")
-    elif wrong == 3:
-        print("\n----")
-        print("  |  |")
-        print("  O  |")
-        print("  |  |")
-        print("     |")
-        print("     |")
-        print("   -----")
-    elif wrong == 4:
-        print("\n----")
-        print("  |  |")
-        print("  O  |")
-        print(" /|  |")
-        print("     |")
-        print("     |")
-        print("   -----")
-    elif wrong == 5:
-        print("\n----")
-        print("  |  |")
-        print("  O  |")
-        print(" /|| |")
-        print("     |")
-        print("     |")
-        print("   -----")
-    elif wrong == 6:
-        print("\n----")
-        print("  |  |")
-        print("  O  |")
-        print(" /|| |")
-        print(" /   |")
-        print("     |")
-        print("   -----")
-    elif wrong == 7:
-        print("\n----")
-        print("  |  |")
-        print("  O  |")
-        print(" /|| |")
-        print(" / | |")
-        print("     |")
-        print("   -----")
+        elif wrong == 1:
+            print("\n----")
+            print("  |  |")
+            print("     |")
+            print("     |")
+            print("     |")
+            print("     |")
+            print("   -----")
+        elif wrong == 2:
+            print("\n----")
+            print("  |  |")
+            print("  O  |")
+            print("     |")
+            print("     |")
+            print("     |")
+            print("   -----")
+        elif wrong == 3:
+            print("\n----")
+            print("  |  |")
+            print("  O  |")
+            print("  |  |")
+            print("     |")
+            print("     |")
+            print("   -----")
+        elif wrong == 4:
+            print("\n----")
+            print("  |  |")
+            print("  O  |")
+            print(" /|  |")
+            print("     |")
+            print("     |")
+            print("   -----")
+        elif wrong == 5:
+            print("\n----")
+            print("  |  |")
+            print("  O  |")
+            print(" /|| |")
+            print("     |")
+            print("     |")
+            print("   -----")
+        elif wrong == 6:
+            print("\n----")
+            print("  |  |")
+            print("  O  |")
+            print(" /|| |")
+            print(" /   |")
+            print("     |")
+            print("   -----")
+        elif wrong == 7:
+            print("\n----")
+            print("  |  |")
+            print("  O  |")
+            print(" /|| |")
+            print(" / | |")
+            print("     |")
+            print("   -----")
+
 
 # Print underscores and letters
-        def printWord(guessedLetters):
-            rightLetters = 0
+def printWord(guessedLetters):
+    counter = 0
+    rightLetters = 0
     for char in randomWord:
-        if char in guessedLetters:
-            print(char, end=" ")
+        if (char in guessedLetters):
+            print(randomWord[counter], end=" ")
             rightLetters += 1
         else:
-            print("_", end=" ")
+            print(" ", end=" ")
+        counter += 1
     return rightLetters
 
+
 # Display words underscore
-    def printUnderscores():
-        print("\r")
+def printUnderscores():
+    print("\r")
     for char in randomWord:
         print("\u203E", end=" ")
+
 
 # Lenght of word
 length_of_word_to_guess = len(randomWord)
@@ -113,25 +118,24 @@ current_letters_right = 0
 while (amount_of_times_wrong != 7 and
        current_letters_right != length_of_word_to_guess):
     print("\nLetters guessed: ")
-for letter in current_letters_guessed:
-    print(letter, end=" ")
+    for letter in current_letters_guessed:
+        print(letter, end=" ")
+
 
 # Request input from the user
-letterGuessed = input("\nGuess a letter: ")
+    letterGuessed = input("\nGuess a letter: ")
 
 # See if guessed letter is correct
-if (randomWord[current_guess_index] == letterGuessed):
-    print_hangman(amount_of_times_wrong)
-    current_guess_index += 1
-    current_letters_guessed.append(letterGuessed)
-    current_letters_right = printWord(current_letters_guessed)
-    printUnderscores()
-    # If not
-else:
-    amount_of_times_wrong += 1
-    current_letters_guessed.append(letterGuessed)
-    print_hangman(amount_of_times_wrong)
-    current_letters_right = printWord(current_letters_guessed)
-    printUnderscores()
+    if (randomWord[current_guess_index] == letterGuessed):
+        print_hangman(amount_of_times_wrong)
+        current_guess_index += 1
+        current_letters_guessed.append(letterGuessed)
+        current_letters_right = printWord(current_letters_guessed)
+        printUnderscores()
+    else:
+        amount_of_times_wrong += 1
+        print_hangman(amount_of_times_wrong)
+        current_letters_right = printWord(current_letters_guessed)
+        printUnderscores()
 
 print("Game Over ")
