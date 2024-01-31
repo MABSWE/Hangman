3c67545d50d0f3cfd132bb756545c01af1fb23aa
MABSWE <>
2024-01-31 10:38:23 +0000

    Add the rest of hangman when wrong

--- run.py
+++ run.py
@@ -15,11 +15,67 @@ for x in randomWord:
 
 # Hangman design when wrong
 def print_hangman(wrong):
-    if(wrong) == 0):
+    if wrong == 0:
         print("\n----")
         print("     |")
         print("     |")
         print("     |")
         print("     |")
+        print("     |")
+        print("   -----")
+    elif wrong == 1:
+        print("\n----")
+        print("  |   |")
+        print("     |")
+        print("     |")
+        print("     |")
+        print("     |")
+        print("   -----")
+    elif wrong == 2:
+        print("\n----")
+        print("  |   |")
+        print("  O   |")
+        print("     |")
+        print("     |")
+        print("     |")
+        print("   -----")
+    elif wrong == 3:
+        print("\n----")
+        print("  |   |")
+        print("  O   |")
+        print("  |   |")
+        print("     |")
+        print("     |")
+        print("   -----")
+    elif wrong == 4:
+        print("\n----")
+        print("  |   |")
+        print("  O   |")
+        print(" / |   |")
+        print("     |")
+        print("     |")
+        print("   -----")
+    elif wrong == 5:
+        print("\n----")
+        print("  |   |")
+        print("  O   |")
+        print(" / | \  |")
+        print("     |")
+        print("     |")
+        print("   -----")
+    elif wrong == 6:
+        print("\n----")
+        print("  |   |")
+        print("  O   |")
+        print(" / | \  |")
+        print("  /   |")
+        print("     |")
+        print("   -----")
+    elif wrong == 7:
+        print("\n----")
+        print("  |   |")
+        print("  O   |")
+        print(" / | \  |")
+        print("  / \  |")
         print("     |")
         print("   -----")
